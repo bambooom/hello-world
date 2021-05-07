9e5ef40044c9b2d74b0c892f8e47ff799ad131c0
zhuzi <nmz89825@gmail.com>
2021-05-08 00:16:45 +0800

    ex17-extra: add some ref for extra 2.

---
+++ C/ex17-extra-dynamic-size.c
@@ -0,0 +1,12 @@
+// Change the code to accept parameters for MAX_DATA and MAX_ROWS,
+// store them in the Database struct, and write that to the file,
+// thus creating a database that can be arbitrarily sized.
+
+/*
+  Ref:
+  - https://stackoverflow.com/questions/21269700/dynamically-sized-structs-learn-c-the-hard-way-ex17
+  - https://github.com/Frederick-S/Learn-C-The-Hard-Way-Exercise/tree/master/Ex17
+  - https://pastebin.com/MvLXkDCz
+  - https://stackoverflow.com/questions/15401954/structs-containing-a-dynamic-array
+  - https://forum.learncodethehardway.com/t/ex17-dynamic-resizing-of-max-data-problem/1516
+*/
