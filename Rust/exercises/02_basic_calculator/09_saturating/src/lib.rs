a50695ef23196c0affd28b967f1b142b4e05fbd1
bamboo <nmz89825@gmail.com>
2025-12-15 11:09:14 +0800

    finish 2.9 saturating.

--- Rust/exercises/02_basic_calculator/09_saturating/src/lib.rs
+++ Rust/exercises/02_basic_calculator/09_saturating/src/lib.rs
@@ -1,9 +1,28 @@
+// overflow-checks is global setting, sometimes need different operations based on the context
+//
+// opt into wrapping arithmetic on a per-operation basis by using the `wrapping_` methods
+//
+// let x = 255u8;
+// let y = 1u8;
+// let sum = x.wrapping_add(y);
+// assert_eq!(sum, 0);
+//
+// opt into saturating arithmetic by using the `saturating_` methods
+// Instead of wrapping around, saturating arithmetic will return the maximum or minimum value for the integer type.
+//
+// let x = 255u8;
+// let y = 1u8;
+// let sum = x.saturating_add(y);
+// assert_eq!(sum, 255);
+//            ⬆️ 255 + 1 = 256, but 256 is bigger than the u8::MAX, the result is u8::MAX (255)
+
+
 pub fn factorial(n: u32) -> u32 {
-    let mut result = 1;
+    let mut result: u32 = 1;
     for i in 1..=n {
         // Use saturating multiplication to stop at the maximum value of u32
         // rather than overflowing and wrapping around
-        result *= i;
+        result = result.saturating_mul(i);
     }
     result
 }
