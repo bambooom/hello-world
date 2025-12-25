fa5cc33709a8653f958a8871d8ff349e6a0ce206
bamboo <nmz89825@gmail.com>
2025-12-25 12:23:31 +0800

    finish 4.10-4.12

--- Rust/exercises/04_traits/10_assoc_vs_generic/src/lib.rs
+++ Rust/exercises/04_traits/10_assoc_vs_generic/src/lib.rs
@@ -13,6 +13,34 @@
 // You don't have to though: it's perfectly okay to write three separate
 // implementations manually. Venture further only if you're curious.
 
+// like `Add` trait
+
+pub trait Power<Exponent = Self> {
+    type Output;
+    fn power(&self, n: Exponent) -> Self::Output;
+}
+
+impl Power<u16> for u32 {
+    type Output = u32;
+    fn power(&self, n: u16) -> Self::Output {
+        self.pow(n.into())
+    }
+}
+
+impl Power<&u32> for u32 {
+    type Output = u32;
+    fn power(&self, n: &u32) -> Self::Output {
+        self.pow(*n as u32)
+    }
+}
+
+impl Power<u32> for u32 {
+    type Output = u32;
+    fn power(&self, n: u32) -> Self::Output {
+        self.pow(n)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::Power;
--- Rust/exercises/04_traits/12_copy/src/lib.rs
+++ Rust/exercises/04_traits/12_copy/src/lib.rs
@@ -1,6 +1,7 @@
 // TODO: implement the necessary traits to make the test compile and pass.
 //  You *can't* modify the test.
 
+#[derive(Copy, Clone, Debug, PartialEq)]
 pub struct WrappingU32 {
     value: u32,
 }
@@ -8,6 +9,21 @@ pub struct WrappingU32 {
 impl WrappingU32 {
     pub fn new(value: u32) -> Self {
         Self { value }
+    }
+}
+
+// Error: implementation of `Add` might be missing for `WrappingU32`
+
+// need to implement std::ops::Add, which is the `+`
+
+impl std::ops::Add for WrappingU32 {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        // return a new instance of WrappingU32
+        // self.value which is u32 instance
+        // and using the wrapping_add method to add two u32 values
+        Self::new(self.value.wrapping_add(rhs.value))
     }
 }
 
