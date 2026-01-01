525517ae90e3671f09d315ef6e68377e68d1a959
bamboo <nmz89825@gmail.com>
2026-01-01 14:51:03 +0800

    finish 5.13-5.15, ch5 done

--- Rust/exercises/05_ticket_v2/14_source/src/lib.rs
+++ Rust/exercises/05_ticket_v2/14_source/src/lib.rs
@@ -1,4 +1,4 @@
-use crate::status::Status;
+use crate::status::{ParseStatusError, Status}; // need to like import ParseStatusError
 
 // We've seen how to declare modules in one of the earliest exercises, but
 // we haven't seen how to extract them into separate files.
@@ -23,6 +23,8 @@ pub enum TicketNewError {
     DescriptionCannotBeEmpty,
     #[error("Description cannot be longer than 500 bytes")]
     DescriptionTooLong,
+    #[error("{0}")] // 0 is the first parameter of the error, in this case ParseStatusError
+    InvalidStatus(#[from] ParseStatusError), // from source
 }
 
 #[derive(Debug, PartialEq, Clone)]
@@ -47,7 +49,7 @@ impl Ticket {
             return Err(TicketNewError::DescriptionTooLong);
         }
 
-        // TODO: Parse the status string into a `Status` enum.
+        let status = Status::try_from(status)?;
 
         Ok(Ticket {
             title,
--- Rust/exercises/05_ticket_v2/15_outro/src/status.rs
+++ Rust/exercises/05_ticket_v2/15_outro/src/status.rs
@@ -1,10 +1,39 @@
 // TODO: Implement `TryFrom<String>` and `TryFrom<&str>` for the `Status` enum.
 //  The parsing should be case-insensitive.
 
+#[derive(Debug, PartialEq, Clone)]
 pub enum Status {
     ToDo,
     InProgress,
     Done,
+}
+
+#[derive(Debug, thiserror::Error)]
+#[error("`{invalid_status}` is not a valid status. Use one of: ToDo, InProgress, Done")]
+pub struct ParseStatusError {
+    invalid_status: String,
+}
+
+impl TryFrom<String> for Status {
+    type Error = ParseStatusError;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        match value.to_lowercase().as_str() {
+            "todo" => Ok(Status::ToDo),
+            "inprogress" => Ok(Status::InProgress),
+            "done" => Ok(Status::Done),
+            _ => Err(ParseStatusError { invalid_status: value }),
+        }
+    }
+}
+
+impl TryFrom<&str> for Status {
+    type Error = ParseStatusError;
+
+    // TryInto trait is a built-in trait in Rust that allows for safe conversion between different types.
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        value.to_lowercase().try_into() // attempts to perform the conversion and returns a Result type
+    }
 }
 
 #[cfg(test)]
