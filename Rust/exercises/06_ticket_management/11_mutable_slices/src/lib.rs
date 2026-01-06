317a4bca118e5d43eaafe1af5d08a8502e54221f
bamboo <nmz89825@gmail.com>
2026-01-06 11:37:33 +0800

    finish 6.10,6.11

--- Rust/exercises/06_ticket_management/11_mutable_slices/src/lib.rs
+++ Rust/exercises/06_ticket_management/11_mutable_slices/src/lib.rs
@@ -1,6 +1,16 @@
 // TODO: Define a function named `squared` that raises all `i32`s within a slice to the power of 2.
 //  The slice should be modified in place.
 
+// &mut keyword indicates that the function can modify the contents of the slice.
+pub fn squared(slice: &mut [i32]) {
+    // iter_mut() method returns an iterator that yields mutable references to each element in the slice.
+    for i in slice.iter_mut() { // i is a mutable reference to an i32
+        //  *i syntax is used to dereference the mutable reference and access the actual value of the element.
+        *i *= *i; // dereference the mutable reference to get the value, and then modify the value
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
