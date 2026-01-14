4adae84b759844e80ceed81f4322725b2faece76
bamboo <nmz89825@gmail.com>
2026-01-14 12:43:04 +0800

    finish 7.11,7.12

--- Rust/exercises/07_threads/11_locks/src/store.rs
+++ Rust/exercises/07_threads/11_locks/src/store.rs
@@ -28,13 +28,14 @@ impl TicketStore {
             description: ticket.description,
             status: Status::ToDo,
         };
-        todo!();
+        let ticket = Arc::new(Mutex::new(ticket));
+        self.tickets.insert(id, ticket.clone());
         id
     }
 
     // The `get` method should return a handle to the ticket
     // which allows the caller to either read or modify the ticket.
-    pub fn get(&self, id: TicketId) -> Option<todo!()> {
-        todo!()
+    pub fn get(&self, id: TicketId) -> Option<Arc<Mutex<Ticket>>> {
+        self.tickets.get(&id).cloned()
     }
 }
