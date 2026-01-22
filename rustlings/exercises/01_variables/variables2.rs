ccd59d00cae4880e733561032168e4bc63e12b8f
bamboo <nmz89825@gmail.com>
2026-01-22 12:02:47 +0800

    finish 01_variables

--- rustlings/exercises/01_variables/variables2.rs
+++ rustlings/exercises/01_variables/variables2.rs
@@ -1,6 +1,6 @@
 fn main() {
     // TODO: Change the line below to fix the compiler error.
-    let x;
+    let x = 8; // must initialize with a value
 
     if x == 10 {
         println!("x is ten!");
--- rustlings/exercises/01_variables/variables6.rs
+++ rustlings/exercises/01_variables/variables6.rs
@@ -1,5 +1,5 @@
 // TODO: Change the line below to fix the compiler error.
-const NUMBER = 3;
+const NUMBER:i32 = 3; // const must have a type annotation
 
 fn main() {
     println!("Number: {NUMBER}");
