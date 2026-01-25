d9d665003fd087f19020f72f2fa6b7475d61c56f
bamboo <nmz89825@gmail.com>
2026-01-25 16:13:46 +0800

    finish 04_primitive types, 05_vec

--- rustlings/exercises/04_primitive_types/primitive_types2.rs
+++ rustlings/exercises/04_primitive_types/primitive_types2.rs
@@ -16,7 +16,12 @@ fn main() {
     // below with your favorite character.
     // Try a letter, try a digit (in single quotes), try a special character, try a character
     // from a different language than your own, try an emoji 😉
-    // let your_character = '';
+
+    // let your_character = 'N';
+    // let your_character = '2'; // digit in single quotes is also ok, but number with two more digits in single quotes will cause error
+    // let your_character = '😉'; // this emooji ok
+    let your_character = '你'; // this character ok
+    // let your_character = '⛄️'; // this emoji not ok, why
 
     if your_character.is_alphabetic() {
         println!("Alphabetical!");
