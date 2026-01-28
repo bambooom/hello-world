3eb9ff00e1f8b8c332e04d5ad02f627e5217a37b
bamboo <nmz89825@gmail.com>
2026-01-28 12:10:17 +0800

    finish 08_enums

--- rustlings/exercises/08_enums/enums2.rs
+++ rustlings/exercises/08_enums/enums2.rs
@@ -7,6 +7,11 @@ struct Point {
 #[derive(Debug)]
 enum Message {
     // TODO: Define the different variants used below.
+    Resize { width: u64, height: u64 }, // like a struct
+    Move(Point),
+    Echo(String),
+    ChangeColor(u8, u8, u8),
+    Quit,
 }
 
 impl Message {
--- rustlings/exercises/08_enums/enums3.rs
+++ rustlings/exercises/08_enums/enums3.rs
@@ -46,6 +46,13 @@ impl State {
     fn process(&mut self, message: Message) {
         // TODO: Create a match expression to process the different message
         // variants using the methods defined above.
+        match message {
+            Message::Resize { width, height } => self.resize(width, height),
+            Message::Move(point) => self.move_position(point),
+            Message::Echo(s) => self.echo(s),
+            Message::ChangeColor(red, green, blue) => self.change_color(red, green, blue),
+            Message::Quit => self.quit(),
+        }
     }
 }
 
