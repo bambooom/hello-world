1ee251969b1fcb4d5a01c9152d251750cf525687
bamboo <nmz89825@gmail.com>
2026-01-29 12:01:18 +0800

    finish 09

--- rustlings/exercises/09_strings/strings3.rs
+++ rustlings/exercises/09_strings/strings3.rs
@@ -1,13 +1,25 @@
 fn trim_me(input: &str) -> &str {
     // TODO: Remove whitespace from both ends of a string.
+    input.trim()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There are multiple ways to do this.
+
+    // let s2 = String::from(" world!");
+    // let mut s = String::from(input);
+    // s.push_str(&s2);
+    // s
+
+
+    // format!("{}{}", input, " world!")
+
+    format!("{input} world!")
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons".
+    input.replace("cars", "balloons")
 }
 
 fn main() {
